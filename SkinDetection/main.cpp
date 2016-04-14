3405dbc434f2b3e8a16a89de7552dd6ff7bfbbd7
TakuyaASANO <x13005xx@aitech.ac.jp>
2016-04-14 16:01:07 +0900

    Initial Commit

---
+++ SkinDetection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SkinDetection
+//
+//  Created by x13005xx on 2016/04/14.
+//  Copyright © 2016年 TakuyaASANO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
